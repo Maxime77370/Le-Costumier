c93973430b8c4995d60eb7599d691504df22b81a
Maxime.C <maxime@karbon.earth>
2024-04-27 18:29:25 +0200

    feat : add product card list

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { ProductCardList } from '@/components/products/product-card-list/products-card-list'
 import { ProductCarousel } from '@/components/products/products-carousel/product-carousel'
 import { ProductTable } from '@/components/products/products-table/product-table'
 import { fakeProducts } from '@/fakeData'
@@ -14,6 +15,7 @@ function Index() {
       <h2 className='mb-2 mt-4 text-2xl font-semibold'>Featured Products</h2>
       <ProductCarousel products={fakeProducts} />
       <ProductTable products={fakeProducts} />
+      <ProductCardList products={fakeProducts} className='mx-auto w-2/3' />
     </div>
   )
 }
